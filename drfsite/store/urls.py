346e26d0bd1247f155b2d0c305009bf5ed7a631c
okulov99 <okulov.e7@gmail.com>
2024-03-27 20:11:50 +0300

    Добавлен весь основной функционал

--- drfsite/store/urls.py
+++ drfsite/store/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from .views import ProductsAPIView
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from .views import ProductsViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'store', ProductsViewSet)
+
 
 urlpatterns = [
-    path('api/v1/products_list/', ProductsAPIView.as_view())
+    path('api/v1/products/', include(router.urls)),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- drfsite/store/views.py
+++ drfsite/store/views.py
@@ -1,9 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
 from .models import Products
+from .permissions import IsAdminOrReadOnly
 from .serializers import ProductsSerializer
 
 
-class ProductsAPIView(generics.ListAPIView):
+class ProductsAPIPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class ProductsViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
+    pagination_class = ProductsAPIPagination
+    permission_classes = IsAdminOrReadOnly
+
+
+
 
